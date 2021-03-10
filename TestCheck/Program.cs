83484bbdae83ea81b4c77da3d13da255e5c4a03e
Bogdan Mazur <b@bmazur.ru>
2021-03-10 22:46:33 +0300

    #Initial

---
+++ TestCheck/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var messages = new Dictionary<bool, string>()
+                { { false, "No" }, { true, "Yes" } };
+
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            string line = " " + string.Join(" ", array);
+            bool key = Convert.ToBoolean(1 + line.IndexOf(" 0"));
+
+            Console.WriteLine(messages[key]);
+            Console.Read();
+        }
+    }
+}
